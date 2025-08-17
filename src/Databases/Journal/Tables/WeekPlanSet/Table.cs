f1668c4de74740d46d5d8abe0b375d34bdc1f2f2
unknown <ngongocthien56@gmail.com>
2025-08-17 10:50:59 +0700

    Small Fixs

--- src/Databases/Journal/Tables/WeekPlanSet/Table.cs
+++ src/Databases/Journal/Tables/WeekPlanSet/Table.cs
@@ -4,8 +4,7 @@ public class Table
 {
     public Guid Id { get; set; }
     public Guid WeekPlanId { get; set; }
-    public int Rep { get; set; }
-    public TimeSpan HoldingTime { get; set; }
+    public int Value { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime LastUpdated { get; set; }
 }
