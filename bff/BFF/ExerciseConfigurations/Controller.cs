539a7a0b791367f9ec03b9feba0c747516cd4a4d
“KhoaVD12” <duykhoa2kk@gmail.com>
2025-10-14 10:33:11 +0700

    Remake Detail

--- bff/BFF/ExerciseConfigurations/Controller.cs
+++ bff/BFF/ExerciseConfigurations/Controller.cs
@@ -1,6 +1,5 @@
 ﻿using BFF.Databases.App;
-using BFF.ExerciseConfigurations.WorkoutLoad;
-using BFF.Models.PaginationResults;
+using BFF.ExerciseConfigurations.Detail;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -78,166 +77,64 @@ namespace BFF.ExerciseConfigurations
             return Ok(workout.Id);
         }
 
-        [HttpGet("workout-load")]
-        public async Task<IActionResult> GetWorkouts([FromQuery] Parameters parameters)
+        [HttpGet("detail")]
+        public async Task<IActionResult> Detail([FromQuery] Parameters parameters)
         {
             var query = _context.Workouts.AsQueryable();
 
-            if (parameters.Id.HasValue)
-                query = query.Where(x => x.Id == parameters.Id);
             if (parameters.ExerciseId.HasValue)
                 query = query.Where(x => x.ExerciseId == parameters.ExerciseId);
             if (parameters.UserId.HasValue)
                 query = query.Where(x => x.UserId == parameters.UserId);
-            if (parameters.CreatedDate.HasValue)
-                query = query.Where(x => x.CreatedDate == parameters.CreatedDate);
-            if (parameters.LastUpdated.HasValue)
-                query = query.Where(x => x.LastUpdated == parameters.LastUpdated);
+            
+            var result = await query.AsNoTracking().FirstOrDefaultAsync();
 
-            if (parameters.PageSize.HasValue && parameters.PageIndex.HasValue && parameters.PageSize > 0 && parameters.PageIndex.Value >= 0)
-                query = query.Skip(parameters.PageSize.Value * parameters.PageIndex.Value).Take(parameters.PageSize.Value);
-
-            var result = await query.AsNoTracking().ToListAsync();
-
-            // Initialize responses list outside the if condition
-            List<Response> responses = result.Select(item => new WorkoutLoad.Response
+            Response response =  new()
             {
-                Id = item.Id,
-                ExerciseId = item.ExerciseId,
-                UserId = item.UserId,
-                CreatedDate = item.CreatedDate,
-                LastUpdated = item.LastUpdated,
-                Exercise = null,
-                WeekPlans = null
-            }).ToList();
+                WorkoutId = result!.Id,
+                UserId = result.UserId,
+            };
 
-            // Dynamically apply Include for valid navigation properties
-            Dictionary<Guid, Exercise> exercisesByExerciseId = new();
-            var exerciseIds = result.Select(w => w.ExerciseId).Distinct().ToList();
-            var exercises = await _context.Exercises
-                .Where(e => exerciseIds.Contains(e.Id))
+            //PercentageCompletion and Difficulty are not implemented yet
+
+            //Get Exercise details
+            var exercise = await _context.Exercises
+                .Where(e => e.Id == result.ExerciseId)
+                .FirstOrDefaultAsync();
+            response.Exercise = new Exercise()
+            {
+                Id=exercise!.Id,
+                Name=exercise!.Name,
+            };
+
+            //Get Muscles for the Exercise
+            var exerciseMuscles = await _context.ExerciseMuscles
+                .Where(m => m.ExerciseId==result.ExerciseId)
                 .ToListAsync();
-
-            // Fetch exercise muscles 
-            Dictionary<Guid, List<Muscle>> musclesByExerciseId = new();
-            var exerciseMuscleRelations = await _context.ExerciseMuscles
-                    .Where(em => exerciseIds.Contains(em.ExerciseId))
-                    .ToListAsync();
-
-            var muscleIds = exerciseMuscleRelations.Select(em => em.MuscleId).Distinct().ToList();
+            var muscleIds=exerciseMuscles.Select(em=>em.MuscleId).ToList();
             var muscles = await _context.Muscles
                 .Where(m => muscleIds.Contains(m.Id))
-                .ToDictionaryAsync(m => m.Id);
-            // Group muscles by exercise ID
-            foreach (var relation in exerciseMuscleRelations)
-            {
-                if (!musclesByExerciseId.ContainsKey(relation.ExerciseId))
-                    musclesByExerciseId[relation.ExerciseId] = new List<Muscle>();
-
-                if (muscles.TryGetValue(relation.MuscleId, out var muscle))
-                {
-                    musclesByExerciseId[relation.ExerciseId].Add(new WorkoutLoad.Muscle
-                    {
-                        Id = muscle.Id,
-                        Name = muscle.Name,
-                        CreatedDate = muscle.CreatedDate,
-                        LastUpdated = muscle.LastUpdated
-                    });
-                }
-            }
-
-            // Create exercise models with their muscles
-            foreach (var exercise in exercises)
-            {
-                exercisesByExerciseId[exercise.Id] = new WorkoutLoad.Exercise
-                {
-                    Id = exercise.Id,
-                    Name = exercise.Name,
-                    Description = exercise.Description,
-                    Type = exercise.Type,
-                    CreatedDate = exercise.CreatedDate,
-                    LastUpdated = exercise.LastUpdated,
-                    Muscles = musclesByExerciseId.ContainsKey(exercise.Id)
-                             ? musclesByExerciseId[exercise.Id]
-                             : null
-                };
-            }
-
-            // Update responses with exercises
-            foreach (var response in responses)
-            {
-                if (exercisesByExerciseId.ContainsKey(response.ExerciseId))
-                {
-                    response.Exercise = exercisesByExerciseId[response.ExerciseId];
-                }
-            }
-
-            Dictionary<Guid, List<WeekPlan>> workoutWeekPlans = new();
-            var workoutIds = result.Select(w => w.Id).ToList();
-            var weekPlans = await _context.WeekPlans
-                .Where(wp => workoutIds.Contains(wp.WorkoutId))
                 .ToListAsync();
-
-            // Fetch WeekPlanSets 
-            Dictionary<Guid, List<WeekPlanSet>> weekPlanSetsByWeekPlanId = new();
-            var weekPlanIds = weekPlans.Select(wp => wp.Id).ToList();
-            var weekPlanSets = await _context.WeekPlanSets
-                        .Where(wps => weekPlanIds.Contains(wps.WeekPlanId))
-                        .ToListAsync();
-
-            foreach (var set in weekPlanSets)
+            response.Exercise.Muscles = [.. muscles.Select(m => new Muscle
             {
-                if (!weekPlanSetsByWeekPlanId.ContainsKey(set.WeekPlanId))
-                    weekPlanSetsByWeekPlanId[set.WeekPlanId] = new List<WeekPlanSet>();
+                Name = m.Name
+            })];
 
-                weekPlanSetsByWeekPlanId[set.WeekPlanId].Add(new WorkoutLoad.WeekPlanSet
-                {
-                    Id = set.Id,
-                    Value = set.Value,
-                    WeekPlanId = set.WeekPlanId,
-                    InsertedBy = set.InsertedBy,
-                    UpdatedBy = set.UpdatedBy,
-                    LastUpdated = set.LastUpdated,
-                    CreatedDate = set.CreatedDate
-                });
-            }
-            // Group week plans by workout ID
-            foreach (var weekPlan in weekPlans)
+            //Get WeekPlans and WeekPlanSets
+            var weekPlans =await _context.WeekPlans.Where(w=>w.WorkoutId==result.Id).ToListAsync();
+            var weekPlanSets=await _context.WeekPlanSets.Where(ws=>weekPlans.Select(w=>w.Id).Contains(ws.WeekPlanId)).ToListAsync();
+            response.WeekPlans = [.. weekPlans.Select(wp => new WeekPlan
             {
-                if (!workoutWeekPlans.ContainsKey(weekPlan.WorkoutId))
-                    workoutWeekPlans[weekPlan.WorkoutId] = new List<WeekPlan>();
-
-                var weekPlanModel = new WorkoutLoad.WeekPlan
+                DateOfWeek = wp.DateOfWeek,
+                WeekPlanSets = [.. weekPlanSets
+                .Where(wps => wps.WeekPlanId == wp.Id)
+                .Select(wps => new WeekPlanSet
                 {
-                    Id = weekPlan.Id,
-                    DateOfWeek = weekPlan.DateOfWeek,
-                    Time = weekPlan.Time,
-                    WorkoutId = weekPlan.WorkoutId,
-                    CreatedDate = weekPlan.CreatedDate,
-                    LastUpdated = weekPlan.LastUpdated,
-                    WeekPlanSets = weekPlanSetsByWeekPlanId.ContainsKey(weekPlan.Id)
-                                  ? weekPlanSetsByWeekPlanId[weekPlan.Id]
-                                  : null
-                };
-                workoutWeekPlans[weekPlan.WorkoutId].Add(weekPlanModel);
-            }
-            // Update responses with week plans
-            foreach (var response in responses)
-            {
-                if (workoutWeekPlans.ContainsKey(response.Id))
-                {
-                    response.WeekPlans = workoutWeekPlans[response.Id];
-                }
-            }
+                    Value = wps.Value
+                })]
+            })];
 
-            var paginationResults = new Builder<Response>()
-                .WithIndex(parameters.PageIndex)
-                .WithSize(parameters.PageSize)
-                .WithTotal(responses.Count)
-                .WithItems(responses)
-                .Build();
-
-            return Ok(paginationResults);
+            return Ok(response);
         }
     }
 }
